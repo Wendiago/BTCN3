c83062378a130c069bd0c83558f495ce29d7a463
21120297 <21120297@student.hcmus.edu.vn>
2023-11-25 18:44:31 +0700

    database create

---
+++ server.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const app = express();
+const port = process.env.PORT;
+const createDB = require('./utils/dbutil');
+
+async function startApp() {
+  // Parsing middleware
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.static(path.join(__dirname, 'public')));
+
+  // Template engine
+  app.engine('.hbs', handlebars.engine);
+  app.set('view engine', '.hbs');
+  app.set('views', path.join(__dirname, '/views'));
+
+  app.use((err, req, res, next) => {
+    const statusCode = err.statusCode | 500;
+    res.status(statusCode).send(err.message);
+  });
+
+  try {
+    await createDB();
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  } catch (error) {
+    console.error('Error starting the app:', error);
+  }
+}
+
+startApp();
---
+++ utils/dbutil.js
@@ -0,0 +1,115 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+async function createDB() {
+  const db = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DB, 
+    password: process.env.DB_PW,
+    port: process.env.DB_PORT,
+  });
+
+  try {
+    await db.connect();
+
+    // Check if the specified database exists
+    const result = await db.query(`SELECT 1 FROM pg_database WHERE datname = '${process.env.DB_NAME}';`);
+    if (result.rowCount === 0) {
+      // Create the specified database if it doesn't exist
+      await db.query(`CREATE DATABASE ${process.env.DB_NAME};`);
+      console.log(`Database ${process.env.DB_NAME} created successfully`);
+    } else {
+      console.log(`Database ${process.env.DB_NAME} already exists`);
+    }
+
+    // Connect to the newly created database
+    const pool = new Pool({
+      user: process.env.DB_USER,
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      password: process.env.DB_PW,
+      port: process.env.DB_PORT,
+    });
+
+    await pool.connect();
+    console.log(`Connected to database ${process.env.DB_NAME}`);
+
+    // Now, create the necessary tables
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS Movie (
+      movie_id VARCHAR(255) PRIMARY KEY,
+      title VARCHAR(255),
+      originalTitle VARCHAR(255),
+      fullTitle VARCHAR(255),
+      year VARCHAR(4),
+      releaseDate DATE,
+      runtimeStr TEXT,
+      plot TEXT,
+      awards VARCHAR(255),
+      directorList TEXT[],
+      writerList TEXT[],
+      actorList JSON[],
+      genreList TEXT[],
+      companies VARCHAR(255),
+      countries VARCHAR(255),
+      languages VARCHAR(255),
+      imDbRating VARCHAR(10),
+      posters TEXT[],
+      images JSON[],
+      boxOffice VARCHAR(255),
+      plotFull TEXT,
+      similars TEXT[]
+    );
+  `);
+
+    // Create table for actors
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS Actor (
+        actor_id VARCHAR(255) PRIMARY KEY,
+        name VARCHAR(255),
+        role VARCHAR(255),
+        image VARCHAR(255),
+        summary TEXT,
+        birthDate DATE,
+        deathDate DATE,
+        awards VARCHAR(255),
+        height VARCHAR(255),
+        castMovies JSON[],
+        images JSON[]
+        );
+    `);
+
+    // Create table for reviews
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS Review (
+        review_id SERIAL PRIMARY KEY,
+        movie_id VARCHAR(255) REFERENCES Movie(movie_id),
+        username VARCHAR(255),
+        warningSpoilers BOOLEAN,
+        date DATE,
+        rate VARCHAR(5),
+        title VARCHAR(255),
+        content TEXT
+        );
+    `);
+
+    // Create table for favorite movies
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS FavoriteMovie (
+        fav_id SERIAL PRIMARY KEY,
+        movie_id VARCHAR(255) REFERENCES Movie(movie_id) ON DELETE CASCADE
+        );
+    `);
+
+        console.log('Tables created successfully');
+        pool.end();
+        db.end();
+        console.log('Connection to the database closed');
+    } catch (err) {
+        console.error('Error occurred:', err);
+        await db.end(); // Close the connection
+    }
+}
+
+module.exports = createDB;
